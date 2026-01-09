133c9c1d631bc6863741819b5b69f0274e5b1800
Rojan Shrestha <pheonixeder@gmail.com>
2026-01-09 12:23:59 +0545

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/weather-app-react",
+  base: process.env.VITE_BASE_PATH ||  "/weather-app-react",
 })
